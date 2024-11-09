7682214496ec6f7c7dd9089ef70f58663a1fd8e0
Anmol <ainsa2279@gmail.com>
2024-11-10 02:18:05 +0530

    💩 Removed

--- src/controller/metaController.ts
+++ src/controller/metaController.ts
@@ -1,29 +1,10 @@
 import { createFactory } from "hono/factory";
-import { Dept } from "../model";
 
 const { createHandlers } = createFactory();
 
 const getMetaHndlr = createHandlers(async (c) => {
-  const depts = await Dept.aggregate([
-    {
-      $addFields: {
-        label: "$name",
-        value: "$_id",
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        name: 0,
-        createdAt: 0,
-        updatedAt: 0,
-        createdBy: 0,
-      },
-    },
-  ]);
-
   return c.json({
-    data: { depts },
+    data: [],
   });
 });
 
