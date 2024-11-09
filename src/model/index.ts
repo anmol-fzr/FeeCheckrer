764a5a07bb95cb06e0a4826111bf5bedd0095913
Anmol <ainsa2279@gmail.com>
2024-11-10 01:18:48 +0530

    Removed Unused Code

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,5 +1,3 @@
 export * from "./user.model";
-export * from "./dept.model";
-export * from "./course.model";
 export * from "./student.model";
 export * from "./fee.model";
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,4 @@
 export * from "./auth.schema";
-export * from "./dept.schema";
 export * from "./env.schema";
 export * from "./user.schema";
-export * from "./course.schema";
 export * from "./student";
